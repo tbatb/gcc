16f6f9fb2376a374b3d34bce23888f9e212e0169
Tegshbayar Batbayar <tegshbayar.batbayar@outlook.com>
2025-09-30 22:00:54 +0200

    Initial commit

---
+++ library.c
@@ -0,0 +1,13 @@
+#include "library.h"
+#include <stdio.h>
+
+void hello(void) {
+    printf("Hello, World!\n");
+
+}
+
+
+int main(){
+    printf("Hello, World for lowlevel code!\n");
+    return 0;
+}
